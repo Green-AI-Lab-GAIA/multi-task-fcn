3fb5d5cecbaf09b631399d8ee3b35389242cf448
luizfernando608 <ziulluz@gmail.com>
2024-06-15 11:20:04 -0300

    refactor: Update batch normalization layer argument name in main.py

--- main.py
+++ main.py
@@ -350,7 +350,7 @@ def train_epochs(last_checkpoint:str,
                                  lr_schedule=lr_schedule, 
                                  figures_path=figures_path, 
                                  lambda_weight=args.lambda_weight,
-                                 batch_norm_layer=args.batch_norm_layer,)
+                                 batch_norm_layer=args.batch_norm,)
         
         logger.info("Evaluating the model...")
         f1_avg, f1_by_class_avg = eval(val_loader, model)
